4c223c02c27268635152db112a12f39eacfac217
AhmedAlsudairi <AhmedAlsudairi@users.noreply.github.com>
2020-07-11 23:04:50 +0300

    second commit

--- src/server/index.js
+++ src/server/index.js
@@ -1,11 +1,29 @@
+const dotenv = require('dotenv');
+dotenv.config({path:'../../to/envVariables.env'});
+// process.env.key is undefiend !!
 var path = require('path')
 const express = require('express')
 const mockAPIResponse = require('./mockAPI.js')
+const bodyParser = require('body-parser')
+const cors = require('cors');
+var AYLIENTextAPI = require('aylien_textapi');
 
 const app = express()
 
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+app.use(cors())
+
 app.use(express.static('dist'))
 
+console.log(`Your API key is ${process.env.API_KEY}`);
+
+var textapi = new AYLIENTextAPI({
+    application_id: process.env.API_ID,
+    application_key: process.env.API_KEY
+  });
+
+  
 console.log(__dirname)
 
 app.get('/', function (req, res) {
